bdc1588a3a71187f6dfcc86329df61a628ce0476
Vikas <21singhvikas@gmail.com>
2019-05-16 15:05:04 +0530

    Add project files.

---
+++ Xamls/Xamls/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamls.Models;
+
+namespace Xamls.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
